04c96590d041454052d01d221c48bb99fdf31c8f
Maciej Madejsza <maciej.madejsza@gmail.com>
2024-09-13 11:55:33 +0100

    modal closure functionality

--- src/services/app.js
+++ src/services/app.js
@@ -2,3 +2,34 @@ function toggleMenu() {
 	const hamburger = document.getElementById('burger');
 	hamburger.classList.toggle('active');
 }
+
+const campTiles = document.querySelectorAll('.tile.camp');
+const body = document.body;
+
+const showModal = (event) => {
+	const tile = event.currentTarget;
+	const targetModal = tile.querySelector('.modal');
+	body.classList.toggle('stopScroll');
+	if (targetModal) {
+		targetModal.classList.add('active');
+		targetModal.scrollTop = 0;
+	}
+};
+
+const closeModal = (event) => {
+	event.stopPropagation(); // Zapobiega zamknięciu modala przez kliknięcie elementu `.tile.camp`
+	const btn = event.currentTarget;
+	const modal = btn.closest('.modal');
+	if (modal) {
+		modal.classList.remove('active');
+	}
+	body.classList.toggle('stopScroll');
+};
+
+campTiles.forEach((camp) => {
+	camp.addEventListener('click', showModal);
+	const closeButton = camp.querySelector('.btn_close');
+	if (closeButton) {
+		closeButton.addEventListener('click', closeModal);
+	}
+});
