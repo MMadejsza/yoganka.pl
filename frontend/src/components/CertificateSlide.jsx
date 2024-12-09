d80ef9868b056e6501530735a0cf8ffa3b9e54cd
Maciej Madejsza <maciej.madejsza@gmail.com>
2024-12-09 08:40:26 +0000

    checking if themes exists

--- frontend/src/components/CertificateSlide.jsx
+++ frontend/src/components/CertificateSlide.jsx
@@ -1,22 +1,19 @@
 function CertificateSlide({slideData}) {
+	const {name, instructor, duration, themes} = slideData;
 	const leadingClass = 'certificates';
 	return (
 		<li className='glide__slide'>
 			<div className={`tile ${leadingClass}__tile`}>
-				{slideData.name && (
+				{name && (
 					<h3>
-						<strong className={`${leadingClass}__name`}>{slideData.name}</strong>
+						<strong className={`${leadingClass}__name`}>{name}</strong>
 					</h3>
 				)}
-				{slideData.instructor && (
-					<p className={`${leadingClass}__school`}>{slideData.instructor}</p>
-				)}
-				{slideData.duration && (
-					<p className={`${leadingClass}__duration`}>{slideData.duration}</p>
-				)}
-				{slideData.themes.length > 0 && (
+				{instructor && <p className={`${leadingClass}__school`}>{instructor}</p>}
+				{duration && <p className={`${leadingClass}__duration`}>{duration}</p>}
+				{themes?.length > 0 && (
 					<p className={`${leadingClass}__type`}>
-						<strong>{slideData.themes.join(' | ')}</strong>
+						<strong>{themes.join(' | ')}</strong>
 					</p>
 				)}
 			</div>
