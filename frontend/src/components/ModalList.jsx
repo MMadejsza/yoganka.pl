76f146ca99992fc18a0b1bb90e315813cdacc5e1
Maciej Madejsza <maciej.madejsza@gmail.com>
2024-12-09 14:15:02 +0000

    tidied up

--- frontend/src/components/ModalList.jsx
+++ frontend/src/components/ModalList.jsx
@@ -5,64 +5,41 @@ function ModalList({extraClass, listType, data}) {
 		optional: 'fa-solid fa-plus modal__icon modal-checklist__icon',
 		freeTime: 'fa-brands fa-pagelines modal__icon modal-checklist__icon',
 	};
-	let iconClasses;
-	let content;
 
-	switch (listType) {
-		case 'included':
-			iconClasses = icons.included;
-			break;
-		case 'excluded':
-			iconClasses = icons.excluded;
-			break;
-		case 'optional':
-			iconClasses = icons.optional;
-			break;
-		case 'freeTime':
-			iconClasses = icons.freeTime;
-			content = data.list.map((item, index) => {
-				let dynamicIcon = iconClasses;
-				if (item.status != 'free') {
-					dynamicIcon = item.status == 'optional' ? icons.optional : iconClasses;
-				}
-				return (
-					<li
-						key={index}
-						className='modal-checklist__li'>
-						<i
-							className={dynamicIcon}
-							aria-hidden='true'></i>
-						{item.activity}
-					</li>
-				);
-			});
-			break;
-		default:
-			iconClasses = icons.freeTime;
-			break;
-	}
+	const pickIconClass = (item) => {
+		// if list is freeTime and has various icons inside
+		if (listType === 'freeTime') {
+			// pick either the one which you have to pay for or free
+			return item.status === 'optional' ? icons.optional : icons.freeTime;
+		}
+		// if different list - pick accordingly
+		return icons[listType] || icons.included;
+	};
 
-	if (listType !== 'freeTime') {
-		content = data.list.map((activity, index) => (
+	const content = data.list.map((item, index) => {
+		// if simple list -> item == string otherwise list of objects from freeTime
+		const activity = typeof item === 'string' ? item : item.activity;
+		const iconClass = typeof item === 'string' ? icons[listType] : pickIconClass(item);
+
+		return (
 			<li
 				key={index}
-				className={`modal-checklist__li`}>
+				className='modal-checklist__li'>
 				<i
-					className={iconClasses}
+					className={iconClass}
 					aria-hidden='true'></i>
 				{activity}
 			</li>
-		));
-	}
+		);
+	});
 
-	const dynamicClass = (baseClass, extra) =>
-		`${baseClass} modal-checklist--${extra} modal-checklist--${
-			listType === 'freeTime' ? 'free-time' : listType
-		}`;
+	const dynamicClass = `modal-checklist modal-checklist--${
+		listType == 'freeTime' ? 'free-time' : listType
+	} ${extraClass ?? ''}`;
 
 	return (
 		<>
-			<section className={dynamicClass('modal-checklist', extraClass)}>
+			<section className={dynamicClass}>
 				<h3 className='modal-checklist__title'>{data.title}</h3>
 				<ul className='modal-checklist__list'>{content}</ul>
 			</section>
