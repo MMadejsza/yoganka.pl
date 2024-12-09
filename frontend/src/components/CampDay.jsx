5a09a66df6b8f2976c38e3a57637afb2bdb3e6ed
Maciej Madejsza <maciej.madejsza@gmail.com>
2024-12-09 13:58:19 +0000

    tidied up

--- frontend/src/components/CampDay.jsx
+++ frontend/src/components/CampDay.jsx
@@ -1,19 +1,18 @@
 function CampDay({dayData}) {
+	const {day, ...schedule} = dayData;
 	return (
 		<>
-			<h4 className='modal__title--day'>{dayData.day}</h4>
+			<h4 className='modal__title--day'>{day}</h4>
 			<ul className='modal__list'>
-				{Object.entries(dayData).map(([time, activity], index) => {
-					if (time != 'day') {
-						return (
-							<li
-								key={index}
-								className='modal__li'>
-								<div>{time}</div>
-								<div>{activity}</div>
-							</li>
-						);
-					}
+				{Object.entries(schedule).map(([time, activity], index) => {
+					return (
+						<li
+							key={index}
+							className='modal__li'>
+							<div>{time}</div>
+							<div>{activity}</div>
+						</li>
+					);
 				})}
 			</ul>
 		</>
