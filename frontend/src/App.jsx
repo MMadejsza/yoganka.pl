e8d5be4d921a169d4cd5b691a739c67e048b7d93
Maciej Madejsza <maciej.madejsza@gmail.com>
2024-12-12 20:23:35 +0000

    initial homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import Burger from './components/nav/Burger.jsx';
-import Nav from './components/nav/Nav.jsx';
-import HeaderMain from './components/HeaderMain.jsx';
-import About from './components/About.jsx';
-import OfferSection from './components/OfferSection.jsx';
-import Certificates from './components/Certificates.jsx';
-import Partners from './components/Partners.jsx';
-import Footer from './components/Footer.jsx';
-import FloatingPopUps from './components/FloatingPopUps.jsx';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import HomePage from './pages/HomePage.jsx';
+
+const router = createBrowserRouter([{path: '/', element: <HomePage />}, {}, {}, {}]);
 
 function App() {
 	return (
 		<div className='wrapper'>
-			<Burger />
-			<Nav />
-			<HeaderMain />
-			<About />
-			<OfferSection />
-			<Certificates />
-			<Partners />
-			<Footer />
-			<FloatingPopUps />
+			<RouterProvider router={router} />
 		</div>
 	);
 }
