59d14d245a061aede0b20f7b7740185daf3e4fc3
Maciej Madejsza <maciej.madejsza@gmail.com>
2025-01-08 15:59:40 +0000

    nav opening by touch

--- frontend/src/components/nav/Burger.jsx
+++ frontend/src/components/nav/Burger.jsx
@@ -1,14 +1,55 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Burger() {
 	const [isOpen, setIsOpen] = useState(false);
+	const [startX, setStartX] = useState(null);
+	let isActive = isOpen ? `active` : undefined;
 
+	// Function handling burger click
 	function handleClick() {
-		const nextState = isOpen ? false : true;
-		setIsOpen(nextState);
+		setIsOpen((prevState) => !prevState);
 	}
 
-	let isActive = isOpen ? `active` : undefined;
+	// Function fetching touch position
+	function handleTouchStart(e) {
+		setStartX(e.touches[0].clientX); // Fetch initial touch position
+	}
+
+	// Function handling main movement logic
+	function handleTouchMove(e) {
+		// Fetch current touch position
+		const currentX = e.touches[0].clientX;
+		// If exists and is in the right 20% of the screen (drawer from the right)
+		if (startX !== null && startX / window.innerWidth >= 0.8) {
+			// If drag left is longer than 10% of the screen - open
+			console.log(currentX + startX);
+			if (startX - currentX > window.innerWidth * 0.1) {
+				setIsOpen(true);
+			}
+		}
+		// If drag right is longer than 10% of the screen - close
+		if (currentX - startX > window.innerWidth * 0.1) {
+			setIsOpen(false);
+		}
+	}
+
+	// Function finishing touch and resetting the position
+	function handleTouchEnd() {
+		setStartX(null); // Resetuj początkowy punkt dotyku
+	}
+
+	// Listening to touch for full page
+	useEffect(() => {
+		window.addEventListener('touchstart', handleTouchStart);
+		window.addEventListener('touchmove', handleTouchMove);
+		window.addEventListener('touchend', handleTouchEnd);
+		// remove on unmount
+		return () => {
+			window.removeEventListener('touchstart', handleTouchStart);
+			window.removeEventListener('touchmove', handleTouchMove);
+			window.removeEventListener('touchend', handleTouchEnd);
+		};
+	}, [startX]);
 
 	return (
 		<div
