bbfaef90a8b2c9553acb28c91a989f2da0493f2c
Maciej Madejsza <maciej.madejsza@gmail.com>
2025-01-14 13:28:02 +0000

    camps into section

--- frontend/src/components/CampsIntoSection.jsx
+++ frontend/src/components/CampsIntoSection.jsx
@@ -10,16 +10,7 @@ function CampsIntoSection({isMobile}) {
 				Voluptates culpa neque impedit consequuntur fugiat earum facilis libero atque rerum
 				hic ut enim quibusdam, porro accusantium repellendus quo corrupti quia perspiciatis
 				cupiditate in perferendis et reprehenderit. Repudiandae laudantium iure excepturi
-				dignissimos, temporibus provident! Similique modi voluptatum officiis non excepturi
-				voluptas, quis ullam totam delectus iste, earum sunt consectetur obcaecati aliquam
-				magnam reiciendis quasi quo itaque sapiente explicabo voluptate molestiae! Modi rem
-				nam saepe animi magni assumenda molestiae possimus vitae eaque cupiditate, explicabo
-				debitis! Dignissimos ab numquam fugit, libero quis tempora non perferendis ut beatae
-				cumque vitae, quibusdam minus aliquid nihil repudiandae asperiores quo laborum.
-				Numquam repudiandae excepturi, nostrum tempora eaque natus earum quasi sequi non
-				sint rem, minima ratione, quis eos nam voluptates. Mollitia est eum maxime eos
-				perspiciatis enim deleniti dolor reiciendis a, optio nihil beatae, ab vel sunt
-				eveniet odio aperiam rerum. Cum, aliquid quo! Consectetur.
+				dignissimos, temporibus provident! .
 			</article>
 		</Section>
 	);
