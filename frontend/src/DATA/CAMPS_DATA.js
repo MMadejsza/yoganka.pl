b569c1858be6a396335b66edd60e09f79ca548b3
Maciej Madejsza <maciej.madejsza@gmail.com>
2025-01-30 11:07:58 +0000

    photos moved to another folder

--- frontend/src/DATA/CAMPS_DATA.js
+++ frontend/src/DATA/CAMPS_DATA.js
@@ -595,7 +595,9 @@ export const CAMPS_DATA = [
 		fileName: `camp_kaszuby`,
 		imgPath: `${htmlToImgsPath}/offer/camps/camp_kaszuby/front`,
 		galleryPath: `${htmlToImgsPath}/offer/camps/camp_kaszuby/gallery`,
-		gallerySize: 22,
+		gallerySize: 13,
+		pastGalleryPath: `${htmlToImgsPath}/offer/camps/camp_kaszuby/past_gallery`,
+		pastGallerySize: 22,
 		eventType: ``,
 		extraClass: ``,
 		front: {
@@ -716,7 +718,9 @@ export const CAMPS_DATA = [
 		fileName: `camp_warmia`,
 		imgPath: `${htmlToImgsPath}/offer/camps/camp_warmia/front`,
 		galleryPath: `${htmlToImgsPath}/offer/camps/camp_warmia/gallery`,
-		gallerySize: 24,
+		gallerySize: 13,
+		pastGalleryPath: `${htmlToImgsPath}/offer/camps/camp_kaszuby/past_gallery`,
+		pastGallerySize: 22,
 		eventType: ``,
 		extraClass: `long`,
 		front: {
