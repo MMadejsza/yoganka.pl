0db6dd3a265fb641c09de7593c3fa79afbfbecb4
Maciej Madejsza <maciej.madejsza@gmail.com>
2025-02-09 17:19:47 +0000

    product model to sequelize

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,38 +1,82 @@
-import db from '../utils/db.js';
-
-class Product {
-	constructor(productID, name, type, location, duration, price, totalSpaces, startDate) {
-		// constructor(data) {
-		// Instead of manual this.uID = uID [...]
-		// Object.assign(this,uID, cID, cType, fName, sName, DoB, pContactMethod, loyalty, refSource, notes)
-		// Object.assign(this, data);
-		this.productID = productID;
-		this.name = name;
-		this.type = type;
-		this.location = location;
-		this.duration = duration;
-		this.price = price;
-		this.totalSpaces = totalSpaces;
-		this.startDate = startDate;
-	}
-	save() {
-		// ? as preparation for later on sequelize
-		return db.execute(
-			'INSERT INTO products (Name, Type, Location, Duration, Price, TotalSpaces, StartDate) VALUES (?, ?, ?, ?, ?, ?, ?)',
-			[
-				this.name,
-				this.type,
-				this.location,
-				this.duration,
-				this.price,
-				this.totalSpaces,
-				this.startDate,
-			],
-		);
-	}
-
-	static fetchAll() {
-		return db.execute(`SELECT * FROM products`);
-	}
-}
+import Sequelize from 'sequelize';
+import sequelizeDb from '../utils/db.js';
+const Product = sequelizeDb.define(
+	'product',
+	{
+		ProductID: {
+			type: Sequelize.INTEGER,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		Name: {
+			type: Sequelize.STRING(255),
+			allowNull: true,
+		},
+		Type: {
+			type: Sequelize.STRING(50),
+			allowNull: true,
+		},
+		Location: {
+			type: Sequelize.STRING(255),
+			allowNull: true,
+		},
+		Duration: {
+			type: Sequelize.TIME, //  HH:MM:SS
+			allowNull: true,
+		},
+		Price: {
+			type: Sequelize.DECIMAL(10, 2),
+			allowNull: true,
+		},
+		TotalSpaces: {
+			type: Sequelize.INTEGER,
+			allowNull: true,
+		},
+		StartDate: {
+			type: Sequelize.DATEONLY, //  YYYY-MM-DD
+			allowNull: true,
+		},
+	},
+	{
+		tableName: 'products', // exact mysql table name
+		timestamps: false, // turn off `createdAt` and `updatedAt`
+	},
+);
 export default Product;
+// import db from '../utils/db.js';
+
+// class Product {
+// 	constructor(productID, name, type, location, duration, price, totalSpaces, startDate) {
+// 		// constructor(data) {
+// 		// Instead of manual this.uID = uID [...]
+// 		// Object.assign(this,uID, cID, cType, fName, sName, DoB, pContactMethod, loyalty, refSource, notes)
+// 		// Object.assign(this, data);
+// 		this.productID = productID;
+// 		this.name = name;
+// 		this.type = type;
+// 		this.location = location;
+// 		this.duration = duration;
+// 		this.price = price;
+// 		this.totalSpaces = totalSpaces;
+// 		this.startDate = startDate;
+// 	}
+// 	save() {
+// 		// ? as preparation for later on sequelize
+// 		return db.execute(
+// 			'INSERT INTO products (Name, Type, Location, Duration, Price, TotalSpaces, StartDate) VALUES (?, ?, ?, ?, ?, ?, ?)',
+// 			[
+// 				this.name,
+// 				this.type,
+// 				this.location,
+// 				this.duration,
+// 				this.price,
+// 				this.totalSpaces,
+// 				this.startDate,
+// 			],
+// 		);
+// 	}
+
+// 	static fetchAll() {
+// 		return db.execute(`SELECT * FROM products`);
+// 	}
+// }
--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,12 +1,20 @@
-import mysql from 'mysql2';
+// import mysql from 'mysql2';
 
-// allowing multiple queries without waiting for finishing previous
-const dbPool = mysql.createPool({
+// // allowing multiple queries without waiting for finishing previous
+// const dbPool = mysql.createPool({
+// 	host: 'localhost',
+// 	user: 'admin1',
+// 	database: 'yoganka',
+// 	password: 'admin1',
+// });
+
+// // promise will allow us use promises and use of async task with chaining instead of callbacks
+// export default dbPool.promise();
+
+import Sequelize from 'sequelize';
+
+const db = new Sequelize('yoganka', 'admin1', 'admin1', {
+	dialect: 'mysql',
 	host: 'localhost',
-	user: 'admin1',
-	database: 'yoganka',
-	password: 'admin1',
 });
-
-// promise will allow us use promises and use of async task with chaining instead of callbacks
-export default dbPool.promise();
+export default db;
