7ee40c2507c438f2ddaa40e764fcda698319aa7b
Maciej Madejsza <maciej.madejsza@gmail.com>
2025-02-16 19:26:48 +0000

    adding new suggestion arrow

--- frontend/src/components/b2b/B2BBenefitsSection.jsx
+++ frontend/src/components/b2b/B2BBenefitsSection.jsx
@@ -3,6 +3,8 @@ import {TYPES} from '../../DATA/B2B_DATA.js';
 
 function B2BBenefitsSection() {
 	const classy = 'camps-benefits__';
+	const mediaQuery = window.matchMedia('(max-width: 1025px)');
+	const isMobile = mediaQuery.matches;
 	return (
 		<Section
 			classy='camps-benefits'
@@ -15,6 +17,11 @@ function B2BBenefitsSection() {
 							className={`${classy}bullet-container types__bullet-container`}
 							tabIndex='0'
 							key={index}>
+							{isMobile && (
+								<span className='material-symbols-rounded click-suggestion'>
+									web_traffic
+								</span>
+							)}
 							<article
 								key={benefit.title}
 								className={`${classy}bullet`}>
