41315d485179d847869698c23e73e302f2361a7b
Maciej Madejsza <maciej.madejsza@gmail.com>
2025-02-21 15:52:07 +0000

    styling correction

--- frontend/src/components/login/LoginForm.jsx
+++ frontend/src/components/login/LoginForm.jsx
@@ -16,11 +16,11 @@ function LoginFrom() {
 	// Update on keystroke
 	const handleChange = (e) => {
 		// pulling out current inputs data
-		const {name, value, type, checked} = e.target;
+		const {id, value, type, checked} = e.target;
 		// updating only necessary
 		setFormData((prevState) => ({
 			...prevState,
-			[name]: type === 'checkbox' ? checked : value,
+			[id]: type === 'checkbox' ? checked : value,
 		}));
 
 		console.log(formData.registration);
@@ -47,17 +47,12 @@ function LoginFrom() {
 
 	const type = formData.registration ? 'register' : 'login';
 	const title = formData.registration ? 'Rejestracja:' : 'Logowanie:';
-	const switchTitle = !formData.registration ? 'Zarejestruj się:' : 'Zaloguj się:';
+	const switchTitle = !formData.registration ? 'Zarejestruj się' : 'Zaloguj się';
+	const actionTitle = formData.registration ? 'Zarejestruj się' : 'Zaloguj się';
 
 	return (
 		<>
 			<section className={type}>
-				<button
-					className='form-switch-btn'
-					onClick={handleFormSwitch}>
-					{switchTitle}
-				</button>
-
 				<form
 					action=''
 					onSubmit={(e) => handleSubmit(e, formData)}
@@ -67,36 +62,40 @@ function LoginFrom() {
 						<label htmlFor={`login`}>Login</label>
 						<input
 							type='text'
-							name={`login`}
+							id={`login`}
 							placeholder='Unikatowy'
 							className={`${type}-form__login-input`}
 							onChange={handleChange}
+							autoComplete='username'
 						/>
 					</div>
 					<div className='input-pair'>
 						<label htmlFor={`password`}>Hasło</label>
 						<input
 							type='password'
-							name={`password`}
+							id={`password`}
 							className={`${type}-form__password-input`}
 							onChange={handleChange}
+							autoComplete='current-password'
 						/>
 					</div>
 					<div className='input-pair'>
-						<label htmlFor={`${type}-email`}>Email</label>
+						<label htmlFor={`email`}>Email</label>
 						<input
 							type='email'
-							name={`email`}
+							id={`email`}
 							placeholder='Do autoryzacji'
 							className={`${type}-form__email-input`}
 							onChange={handleChange}
+							autoComplete='email'
 						/>
 					</div>
 				</form>
+				<button className={`modal__btn`}>{actionTitle}</button>
 				<button
-					className={`${type}-btn'`}
-					name={`${type}-btn'`}>
-					{formData.registration ? 'Zarejestruj' : 'Zaloguj'}
+					className='form-switch-btn modal__btn'
+					onClick={handleFormSwitch}>
+					{switchTitle}
 				</button>
 			</section>
 		</>
