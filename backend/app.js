3c51d6ecea125085414c4cf163f659a917820bd4
Maciej Madejsza <maciej.madejsza@gmail.com>
2025-02-23 09:19:16 +0000

    session initialized

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import session from 'express-session';
 import authRoutes from './routes/authRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
 import customerRoutes from './routes/customerRoutes.js';
@@ -9,6 +10,15 @@ const app = express();
 // middleware funnels
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+// secret is used for signing the hash, resave:false - session will not be saved on every request, saveUninitialized: false - that it won't be saved if nothing has changed
+app.use(
+	session({
+		secret: 'my secret',
+		resave: false,
+		saveUninitialized: false,
+		cookie: {maxAge: 86400000}, // 1 day in milliseconds
+	}),
+);
 // app.use(express.static(path.join(rootDir, '../../frontend/public')));
 
 // Filtering that works only for /admin/*
