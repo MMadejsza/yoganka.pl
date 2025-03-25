72ecee284ac1689100cfa8a84c489f65d4e4339d
Maciej Madejsza <maciej.madejsza@gmail.com>
2025-03-25 20:49:12 +0000

    credentials. to .env

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,7 +1,13 @@
+import 'dotenv/config';
 import Sequelize from 'sequelize';
 
-const db = new Sequelize('yoganka', 'admin1', 'admin1', {
-  dialect: 'mysql',
-  host: 'localhost',
-});
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+  }
+);
 export default db;
