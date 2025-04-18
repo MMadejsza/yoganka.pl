3bac6a95bb2a06c59d7cb3a8cebb495c36352d8b
Maciej Madejsza <maciej.madejsza@gmail.com>
2025-04-18 14:15:16 +0100

    Admin side menu styled properly desktop

--- frontend/src/components/backend/SideNav.jsx
+++ frontend/src/components/backend/SideNav.jsx
@@ -62,8 +62,34 @@ function SideNav({ menuSet, side, type, onclose }) {
   };
   return (
     <aside className='side-nav'>
-      <nav className={`nav ${side}`}>
+      <nav className={`nav${side ? ` ${side}` : ''}`}>
         <ul className='nav__list'>
+          <ul className='nav__list nav__list--bottom'>
+            {menuSet.map(li => (
+              <li key={li.name} className='nav__item nav__item--bottom'>
+                <NavLink
+                  to={li.link}
+                  onClick={e => {
+                    if (type == 'action') {
+                      e.preventDefault();
+                      handleOpenModal(pickDestination(location).dest);
+                    }
+                  }}
+                  className={({ isActive }) =>
+                    isActive
+                      ? 'nav__link nav__link--bottom active'
+                      : 'nav__link nav__link--bottom'
+                  }
+                >
+                  {({ isActive }) => (
+                    <span className='nav__content nav__content--bottom'>
+                      {li.name}
+                    </span>
+                  )}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
           {menuSet.map(li => (
             <li key={li.name} className='nav__item'>
               <NavLink
@@ -80,7 +106,6 @@ function SideNav({ menuSet, side, type, onclose }) {
               >
                 {({ isActive }) => (
                   <>
-                    {li.name}
                     <span
                       className={`${li.symbol} nav__icon ${
                         isActive ? 'active' : ''
