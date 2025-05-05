4377ed96c32b6d754b413a10b5fe2c76b0799e50
Maciej Madejsza <maciej.madejsza@gmail.com>
2025-05-05 10:02:38 +0100

    clean up for screenshots

--- backend/middleware/setSession.js
+++ backend/middleware/setSession.js
@@ -15,9 +15,9 @@ const options = {
 const MySQLStore = MySQLStoreFactory(session);
 const sessionStore = new MySQLStore(options);
 
+// secret is used for signing the hash, resave:false - session will not be saved on every request, saveUninitialized: false - that it won't be saved if nothing is stored in this session
 export const setSession = session({
   key: 'session_CID',
-  // secret is used for signing the hash, resave:false - session will not be saved on every request, saveUninitialized: false - that it won't be saved if nothing is stored in this session
   // ! to change
   secret: process.env.SESSION_SECRET,
   resave: false,
@@ -25,8 +25,8 @@ export const setSession = session({
   store: sessionStore,
   cookie: {
     maxAge: 86400000, // 1 day in milliseconds
-    // httpOnly: true,    // Protect from the access from the JavaScript
-    // secure: false,     // Set to true if you use HTTPS
-    // sameSite: 'lax'    // Extra security from CSRF
+    // httpOnly: true,
+    // secure: false,
+    // sameSite: 'lax'
   },
 });
