2dcb48bbdee37e96bcc7fa2d5b993c39bada3446
Maciej Madejsza <maciej.madejsza@gmail.com>
2025-07-11 14:35:55 +0100

    b2b pricelist component done

--- frontend/src/components/frontend/b2b/PriceList.jsx
+++ frontend/src/components/frontend/b2b/PriceList.jsx
@@ -2,30 +2,46 @@ import { Link } from 'react-router-dom';
 import Section from '../../../components/frontend/Section.jsx';
 import SymbolOrIcon from '../../common/SymbolOrIcon';
 
-function PriceList({
-  btns = {},
-  title,
-  desc = [],
-  modifier,
-  children = [],
-  id,
-}) {
+function PriceList({ content, title, desc = [], modifier, children = [], id }) {
   const mediaQuery = window.matchMedia('(max-width: 1024px)');
   const isNotMobile = !mediaQuery.matches;
-  const areBtns = Object.entries(btns).length > 0;
+  const areBtns = Object.entries(content.btnsContent).length > 0;
   const renderBtns = areBtns ? (
     <footer
       className={`modal__user-action ${
         modifier ? `modal__user-action--${modifier}` : ''
       }`}
     >
-      {btns.btnsContent.map((btn, index) => {
+      {content.btnsContent.map((btn, index) => {
         const isIconOrSymbol = btn.icon || btn.symbol;
+        let formattedLink;
+        switch (btn.action) {
+          case 'mail':
+            formattedLink = `mailto:${btn.link}`;
+            btn.symbol = 'mail';
+            btn.text = 'wyślij maila';
+            break;
+          case 'phone':
+            formattedLink = `tel:${btn.link}`;
+            btn.symbol = 'phone';
+            btn.text = 'zadzwoń';
+            break;
+          case 'whatsapp':
+            formattedLink = `https://wa.me/${btn.link}`;
+            btn.icon = 'fa-brands fa-whatsapp';
+            btn.text = 'whatsapp';
+            break;
+
+          default:
+            formattedLink = btn.link;
+            break;
+        }
+
         if (btn.action === 'subPage') {
           return (
             <Link
               key={index + btn.title}
-              to={btn.link}
+              to={formattedLink}
               onClick={() => {
                 window.scrollTo(0, 0);
               }}
@@ -51,7 +67,7 @@ function PriceList({
               }
               key={index + btn.title}
               target='_blank'
-              href={btn.link}
+              href={formattedLink}
               title={btn.title}
               className={`tile__btn modal__btn`}
             >
@@ -68,9 +84,9 @@ function PriceList({
   ) : null;
 
   return (
-    <Section classy='b2b-price' header={title} id={id}>
+    <Section classy='b2b-price' header={content.sectionTitle} id={id}>
       <article className='b2b-price__content'>
-        {desc.map((pContent, index) => (
+        {content.desc.map((pContent, index) => (
           <p key={index} className='b2b-price__p'>
             {pContent}
           </p>
--- frontend/src/pages/frontend/B2BPage.jsx
+++ frontend/src/pages/frontend/B2BPage.jsx
@@ -4,6 +4,7 @@ import { Helmet } from 'react-helmet';
 import Benefits from '../../components/frontend/b2b/B2BBenefits.jsx';
 import B2BOptionsSection from '../../components/frontend/b2b/B2BBenefitsSection.jsx';
 import B2BIntroSection from '../../components/frontend/b2b/B2BIntroSection.jsx';
+import PriceList from '../../components/frontend/b2b/PriceList.jsx';
 import OfferSection from '../../components/frontend/OfferSection.jsx';
 import { client } from '../../utils/sanityClient.js';
 // import { BTNS, OFFER, TYPES } from '../../DATA/B2B_DATA.js';
@@ -46,9 +47,9 @@ function B2BPage() {
   });
 
   // console.log(TYPES);
-  console.log(OFFER);
+  // console.log(OFFER);
   // console.log(BENEFITS);
-  // console.log(PRICE_LIST);
+  console.log(PRICE_LIST);
   let products = null;
 
   if (OFFER) {
@@ -98,17 +99,9 @@ function B2BPage() {
       {OFFER && <OfferSection products={products} />}
       {TYPES && <B2BOptionsSection content={TYPES} />}
       {BENEFITS && <Benefits content={BENEFITS} />}
-      {/* <PriceList
-        title={`Cennik i\u00A0współpraca`}
-        btns={BTNS}
-        desc={[
-          `Oferta jest elastyczna i\u00A0dopasowana do\u00A0potrzeb Twojej firmy.`,
-          `Skontaktuj się\u00A0po szczegóły i\u00A0indywidualną wycenę,
-          a\u00A0wspólnie stworzymy plan, który\u00A0przyniesie najlepsze efekty
-          dla\u00A0Twojego zespołu.`,
-        ]}
-        modifier={'classes-page'}
-      /> */}
+      {PRICE_LIST && (
+        <PriceList content={PRICE_LIST} modifier={'classes-page'} />
+      )}
     </>
   );
 }
