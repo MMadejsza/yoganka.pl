28b7ae3c622b31da69ae2e8e31c0e6455cc04e1f
Maciej Madejsza <maciej.madejsza@gmail.com>
2025-07-15 16:05:23 +0100

    classes commented

--- frontend/src/components/frontend/classes/ClassesIntro.jsx
+++ frontend/src/components/frontend/classes/ClassesIntro.jsx
@@ -1,60 +1,60 @@
-import Section from '../Section.jsx';
-import StampedImg from '../imgsRelated/StampedImg.jsx';
-import img320 from '/imgs/about/480_about_profile.jpg';
-import img600 from '/imgs/about/768_about_profile.jpg';
-import cert320 from '/imgs/about/about_YA-Logo.png';
-import cert600 from '/imgs/about/about_ryt.png';
-import logoImg320 from '/imgs/logo/320_logo_14.png';
-import logoImg480 from '/imgs/logo/480_logo_14.png';
-const logoPaths = [
-  { path: logoImg320, size: '320w' },
-  { path: logoImg480, size: '480w' },
-];
-const stampedImgPaths = [
-  { path: img320, size: '480w' },
-  { path: img600, size: '768w' },
-];
-const stampedImgCertPaths = [
-  { path: cert320, alt: 'Logo federacji yogi' },
-  { path: cert600, alt: 'Autoryzowany nauczyciel jogi' },
-];
-function ClassesIntro({ isMobile }) {
-  const modifier = 'classes-page';
-  const prefix = 'about';
-  const pClass = `${prefix}__bio--description`;
-  return (
-    <Section classy='about' header='Zajęcia jogi' modifier={modifier}>
-      <StampedImg
-        noStamp={true}
-        placement='about'
-        modifier={modifier}
-        imgPaths={stampedImgPaths}
-        certPaths={stampedImgCertPaths}
-        sizes='(max-width: 640px) 320px,(max-width: 1024px) 600px,320px'
-        alt='Yoganka - w pozycji na plaży'
-      />
-      <article
-        className={`${prefix}__bio--content ${
-          modifier ? `${prefix}__bio--content--${modifier}` : ''
-        }`}
-      >
-        <h1
-          className={`${prefix}__bio--heading ${
-            modifier ? `${prefix}__bio--heading--${modifier}` : ''
-          }`}
-        >
-          Jakiej jogi uczę
-        </h1>
-        <p className={pClass}>
-          Prowadzę zajęcia w&nbsp;duchu vinyasa&nbsp;yoga. Nie&nbsp;mniej
-          na&nbsp;przestrzeni doświadczenia, odkrywania co mi&nbsp;służy
-          najbardziej, wykształciłam jej własny styl. Stawiam
-          na&nbsp;wzmacnianie oraz&nbsp;rozciąganie, dzięki czemu kompleksowo
-          dbasz o&nbsp;ciało. Stosuję również techniki
-        </p>
-      </article>
-    </Section>
-  );
-}
+// import Section from '../Section.jsx';
+// import StampedImg from '../imgsRelated/StampedImg.jsx';
+// import img320 from '/imgs/about/480_about_profile.jpg';
+// import img600 from '/imgs/about/768_about_profile.jpg';
+// import cert320 from '/imgs/about/about_YA-Logo.png';
+// import cert600 from '/imgs/about/about_ryt.png';
+// import logoImg320 from '/imgs/logo/320_logo_14.png';
+// import logoImg480 from '/imgs/logo/480_logo_14.png';
+// const logoPaths = [
+//   { path: logoImg320, size: '320w' },
+//   { path: logoImg480, size: '480w' },
+// ];
+// const stampedImgPaths = [
+//   { path: img320, size: '480w' },
+//   { path: img600, size: '768w' },
+// ];
+// const stampedImgCertPaths = [
+//   { path: cert320, alt: 'Logo federacji yogi' },
+//   { path: cert600, alt: 'Autoryzowany nauczyciel jogi' },
+// ];
+// function ClassesIntro({ isMobile }) {
+//   const modifier = 'classes-page';
+//   const prefix = 'about';
+//   const pClass = `${prefix}__bio--description`;
+//   return (
+//     <Section classy='about' header='Zajęcia jogi' modifier={modifier}>
+//       <StampedImg
+//         noStamp={true}
+//         placement='about'
+//         modifier={modifier}
+//         imgPaths={stampedImgPaths}
+//         certPaths={stampedImgCertPaths}
+//         sizes='(max-width: 640px) 320px,(max-width: 1024px) 600px,320px'
+//         alt='Yoganka - w pozycji na plaży'
+//       />
+//       <article
+//         className={`${prefix}__bio--content ${
+//           modifier ? `${prefix}__bio--content--${modifier}` : ''
+//         }`}
+//       >
+//         <h1
+//           className={`${prefix}__bio--heading ${
+//             modifier ? `${prefix}__bio--heading--${modifier}` : ''
+//           }`}
+//         >
+//           Jakiej jogi uczę
+//         </h1>
+//         <p className={pClass}>
+//           Prowadzę zajęcia w&nbsp;duchu vinyasa&nbsp;yoga. Nie&nbsp;mniej
+//           na&nbsp;przestrzeni doświadczenia, odkrywania co mi&nbsp;służy
+//           najbardziej, wykształciłam jej własny styl. Stawiam
+//           na&nbsp;wzmacnianie oraz&nbsp;rozciąganie, dzięki czemu kompleksowo
+//           dbasz o&nbsp;ciało. Stosuję również techniki
+//         </p>
+//       </article>
+//     </Section>
+//   );
+// }
 
-export default ClassesIntro;
+// export default ClassesIntro;
--- frontend/src/components/frontend/classes/ViewClassesOnline.jsx
+++ frontend/src/components/frontend/classes/ViewClassesOnline.jsx
@@ -1,55 +1,55 @@
-import { Helmet } from 'react-helmet';
-import Benefits from '../../../components/frontend/b2b/B2BBenefits.jsx';
-import PriceList from '../../../components/frontend/b2b/PriceList.jsx';
-import OfferSection from '../../../components/frontend/OfferSection.jsx';
-import {
-  BENEFITS_TILES,
-  BTNS,
-  PRICE_LIST,
-} from '../../../DATA/CLASSES_DATA.js';
-import B2BOptionsSection from '../b2b/B2BBenefitsSection.jsx';
-import Section from '../Section.jsx';
+// import { Helmet } from 'react-helmet';
+// import Benefits from '../../../components/frontend/b2b/B2BBenefits.jsx';
+// import PriceList from '../../../components/frontend/b2b/PriceList.jsx';
+// import OfferSection from '../../../components/frontend/OfferSection.jsx';
+// import {
+//   BENEFITS_TILES,
+//   BTNS,
+//   PRICE_LIST,
+// } from '../../../DATA/CLASSES_DATA.js';
+// import B2BOptionsSection from '../b2b/B2BBenefitsSection.jsx';
+// import Section from '../Section.jsx';
 
-const products = [
-  { id: 'zajecia', header: `Cennik`, data: PRICE_LIST, limit: false },
-];
-function ViewClassesOnline() {
-  return (
-    <>
-      <Helmet>
-        <title>Yoganka - Zajęcia online</title>
-        <meta
-          name='description'
-          content='Ćwicz jogę online z Yoganką - zajęcia na żywo przez Zoom, dostępne także w nagraniu przez 48h.'
-        />
-        <link rel='canonical' href='https://yoganka.pl/zajecia/online' />
-      </Helmet>
-      <Section
-        id={'tabs-content'}
-        classy='admin-intro'
-        header={'Studio online'}
-      />
-      <B2BOptionsSection list={BENEFITS_TILES} title={'Dlaczego warto?'} />
-      <Benefits
-      // title=''
-      // modifier='classes-page'
-      // list={[
-      //   `Praktyka na platformie ZOOM`,
-      //   `Nagrywane zajęcia dostępne do 48h\u00a0po`,
-      //   `Czwartki  godz. 19:00, 75min`,
-      //   `Wznowienie jesienią 2025`,
-      // ]}
-      // content={}
-      />
-      <OfferSection products={products} />
-      <PriceList
-        title={`Rezerwuj zajęcia`}
-        btns={BTNS}
-        modifier={'classes-page'}
-        id={'tabs-content'}
-      />
-    </>
-  );
-}
+// const products = [
+//   { id: 'zajecia', header: `Cennik`, data: PRICE_LIST, limit: false },
+// ];
+// function ViewClassesOnline() {
+//   return (
+//     <>
+//       <Helmet>
+//         <title>Yoganka - Zajęcia online</title>
+//         <meta
+//           name='description'
+//           content='Ćwicz jogę online z Yoganką - zajęcia na żywo przez Zoom, dostępne także w nagraniu przez 48h.'
+//         />
+//         <link rel='canonical' href='https://yoganka.pl/zajecia/online' />
+//       </Helmet>
+//       <Section
+//         id={'tabs-content'}
+//         classy='admin-intro'
+//         header={'Studio online'}
+//       />
+//       <B2BOptionsSection list={BENEFITS_TILES} title={'Dlaczego warto?'} />
+//       <Benefits
+//       // title=''
+//       // modifier='classes-page'
+//       // list={[
+//       //   `Praktyka na platformie ZOOM`,
+//       //   `Nagrywane zajęcia dostępne do 48h\u00a0po`,
+//       //   `Czwartki  godz. 19:00, 75min`,
+//       //   `Wznowienie jesienią 2025`,
+//       // ]}
+//       // content={}
+//       />
+//       <OfferSection products={products} />
+//       <PriceList
+//         title={`Rezerwuj zajęcia`}
+//         btns={BTNS}
+//         modifier={'classes-page'}
+//         id={'tabs-content'}
+//       />
+//     </>
+//   );
+// }
 
-export default ViewClassesOnline;
+// export default ViewClassesOnline;
